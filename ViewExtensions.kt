b247539645441f54816262559f12688142f07034
Sayan Porya <37997916+eco4ndly@users.noreply.github.com>
2020-04-03 15:44:21 +0530

    Update ViewExtensions.kt

--- ViewExtensions.kt
+++ ViewExtensions.kt
@@ -18,3 +18,31 @@ fun View.visible() {
 fun View.invisible() {
   visibility = View.INVISIBLE
 }
+
+/**
+* Extension method to inflate a layout file
+* ~BASIC USAGE~
+* val view = parent.inflate(R.layout.layout_file)
+*/
+fun ViewGroup.inflate(@LayoutRes resource: Int, attachToRoot: Boolean = false): View =
+    LayoutInflater.from(context).inflate(resource, this, attachToRoot)
+
+/**
+ * Extension method to show keyboard.
+ */
+fun Activity.showKeyboard(view: View) {
+    val imm: InputMethodManager =
+        getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    imm.showSoftInput(view, InputMethodManager.SHOW_IMPLICIT)
+}
+
+/**
+ * Extension method to hide keyboard.
+ */
+fun Activity.hideSoftKeyboard() {
+    if (currentFocus != null) {
+        val inputMethodManager = getSystemService(Context
+            .INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(currentFocus!!.windowToken, 0)
+    }
+}
