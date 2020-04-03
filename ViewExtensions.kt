2747f4781402a68288d9c17fb6c47775b98c064b
Sayan Porya <37997916+eco4ndly@users.noreply.github.com>
2020-04-03 15:33:21 +0530

    Update ViewExtensions.kt

--- ViewExtensions.kt
+++ ViewExtensions.kt
@@ -1,1 +1,20 @@
+/**
+ * Extension method to make a view gone
+ */
+fun View.gone() {
+  visibility = View.GONE
+}
 
+/**
+ * Extension method to make a view visible.
+ */
+fun View.visible() {
+  visibility = View.VISIBLE
+}
+
+/**
+ * Extension method to make a view invisible
+ */
+fun View.invisible() {
+  visibility = View.INVISIBLE
+}
