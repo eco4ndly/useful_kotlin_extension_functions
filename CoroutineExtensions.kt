56c8bf16c82c61fe8155a15e37a2769485f05d7d
Sayan Porya <37997916+eco4ndly@users.noreply.github.com>
2020-04-13 15:51:59 +0530

    Update CoroutineExtensions.kt
    added clicks extension function for coroutine flow

--- CoroutineExtensions.kt
+++ CoroutineExtensions.kt
@@ -1,1 +1,29 @@
 
+/**
+ * **How to use it**
+ * //In Activity or Fragment
+ * @code
+   private val mainScope = MainScope()
+   val btnClick: Button = findViewById(R.id.btn)
+   btnClick.clicks()
+            .onEach {
+                //DO STUFF ON CLICK
+            }
+            .launchIn(mainScope)
+ */
+@ExperimentalCoroutinesApi
+fun View.clicks(): Flow<Unit> = callbackFlow {
+  val listener = View.OnClickListener { safeOffer(Unit) }
+  setOnClickListener(listener)
+  awaitClose {
+    setOnClickListener(null)
+  }
+}
+
+@ExperimentalCoroutinesApi
+fun <E> SendChannel<E>.safeOffer(value: E) = !isClosedForSend && try {
+  offer(value)
+} catch (t: Throwable) {
+  // Ignore all
+  false
+}
