9a1e6ac1f2344cfe1a83bd26a25c0ee33a7f81c8
Sayan Porya <37997916+eco4ndly@users.noreply.github.com>
2020-04-28 12:38:45 +0530

    Update CoroutineExtensions.kt

--- CoroutineExtensions.kt
+++ CoroutineExtensions.kt
@@ -27,3 +27,53 @@ fun <E> SendChannel<E>.safeOffer(value: E) = !isClosedForSend && try {
   // Ignore all
   false
 }
+
+
+/**
+ * To add FLOW click listener on multiple views at once.
+ * Rather having code like this
+ * @code
+ *    view1.clicks()
+        .onEach {
+          //View 1 Stuffs
+        }
+        .launchIn(mainScope)
+
+      view2.clicks()
+        .onEach {
+          //View 1 Stuffs
+        }
+        .launchIn(mainScope)
+
+      view3.clicks()
+        .onEach {
+          //View 1 Stuffs
+      }
+      .launchIn(mainScope)
+ **************************
+ * Having this |
+ * @code
+ *    clicks(view1, view2, view3, mainScope) { clickedViewId ->
+        when(clickedViewId) {
+          R.id.view1 -> {}
+          R.id.view2 -> {}
+          R.id.view3 -> {}
+        }
+ *    }
+ *******************************
+ *
+ * The code becomes much concise
+ * 
+ * Has dependency on Flow clicks() method 
+ * @link https://github.com/eco4ndly/useful_kotlin_extension_functions/blob/master/CoroutineExtensions.kt
+ */
+fun Any.clicks(vararg views: View, mainScope: CoroutineScope, onClick: (viewId: Int) -> Unit) {
+
+    views.forEach {view ->
+        view.clicks() // Flow Clicks method
+            .onEach {
+                onClick.invoke(view.id)
+            }.launchIn(mainScope)
+    }
+
+}
