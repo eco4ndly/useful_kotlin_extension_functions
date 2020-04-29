294740abc922c6eae71274038e5b9d47ef6fc347
Sayan Porya <37997916+eco4ndly@users.noreply.github.com>
2020-04-29 19:22:39 +0530

    added few more extension functions

--- StringExtensions.kt
+++ StringExtensions.kt
@@ -8,3 +8,17 @@ fun String.tail() = this.substring(1)
 * Returns the first item of a string
 */
 fun String.head() = this.substring(0, 1)
+
+/**
+* Very Very Very Very way to parse a string to date
+* You'll not use this in production, pinky promise
+*/
+fun String.toDate(format: String): Date = SimpleDateFormat(format).parse(this)
+
+/**
+* Normalizes a string
+*/
+fun String.normalizeString() =
+    Normalizer.normalize(this, Normalizer.Form.NFD)
+        .toLowerCase()
+        .replace("\\p{InCombiningDiacriticalMarks}+".toRegex(), "")
